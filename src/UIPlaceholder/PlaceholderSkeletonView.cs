96b82bd2aefeb3fead6aedcb3126c5d38529834d
Damien Delaire <delairedd@gmail.com>
2019-10-10 10:37:34 +0200

    Adding simple loading blinking animation

---
+++ src/UIPlaceholder/PlaceholderSkeletonView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace UIPlaceholder
+{
+    public class PlaceholderSkeletonView : Grid
+    {
+        Storyboard myLoadingStoryboard { get; set; }
+        public PlaceholderSkeletonView()
+        {
+            myLoadingStoryboard = new Storyboard();
+            var blinkAnimation = new DoubleAnimation()
+            {
+                From = 1,
+                To = 0.4,
+                AutoReverse = true,
+                //blink every 1sec
+                Duration = new Windows.UI.Xaml.Duration(TimeSpan.FromSeconds(1)),
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            myLoadingStoryboard.Children.Add(blinkAnimation);
+
+            Storyboard.SetTarget(myLoadingStoryboard, this);
+            Storyboard.SetTargetProperty(blinkAnimation, "Opacity");
+
+            Loaded += (a, e) => myLoadingStoryboard.Begin();
+        }
+    }
+}
