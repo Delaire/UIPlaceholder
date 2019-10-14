3eaafa700d9b66b01ec6e036ea2f46d35f1fe539
Damien Delaire <delairedd@gmail.com>
2019-10-14 09:43:25 +0200

    init how sample should look like

---
+++ sample/UIPlaceholderSample/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIPlaceholder;
+
+namespace UIPlaceholderSample.ViewModel
+{
+    public class MainPageViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        private PlaceholderState _currentState = PlaceholderState.NoState;
+        public PlaceholderState CurrentState
+        {
+            get
+            {
+                return _currentState;
+            }
+            set
+            {
+                Set(ref _currentState, value);
+            }
+        }
+
+
+        public async void LoadDifferentStates()
+        {
+            CurrentState = PlaceholderState.Loading;
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            CurrentState = PlaceholderState.NoState;
+        }
+    }
+}
---
+++ sample/UIPlaceholderSample/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIPlaceholderSample.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+    }
+}
